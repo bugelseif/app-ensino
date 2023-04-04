783edd50d91cab95d809f456e57da3bb1df12773
Bruna Ferreira <bferreira.slv@gmail.com>
2023-04-04 18:57:40 -0300

    chamada da api separada

--- frontend/App.js
+++ frontend/App.js
@@ -1,33 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
+import { get_info } from '/src/services/api.js'
 
 export default function App() { 
-  const [infos, setInfos] = useState(null)
-  
-  const get_info = () => {
-    const endpoint = "http://127.0.0.1:8000/users/" // verificar local da API
-
-    fetch(endpoint)
-    .then(resposta => resposta.json())
-    .then(json =>{
-      console.log(json)
-      let nome = " "
-      for (let i = 0; i<json.length; i++) {
-        console.log("for")
-        nome += json[i].name + " "
-      }
-      console.log(nome)
-
-      setInfos(nome)
-  })
-  .catch(() =>
-    Alert.alert('Erro')
-  )}
-
-  get_info()
-  
+  let infos = get_info()
   console.log(infos)
+
   return(
     <View style={styles.container}>
       <Text>Hello</Text>
@@ -46,5 +25,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
