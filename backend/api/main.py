6923a542a41ceadfaa884b83d987adf4c28911d5
Bruna Ferreira <bferreira.slv@gmail.com>
2023-04-10 16:01:42 -0300

    adicionada tipagem em get/users

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 
+from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
@@ -52,7 +53,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="email already registered")
     return crud.create_user(db=db, user=user)
 
-@app.get("/users/", response_model=list[schemas.User])
+@app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     print(users)
