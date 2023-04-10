660a2cce3b096619d8f22268f19b7174444b0fd6
Bruna Ferreira <bferreira.slv@gmail.com>
2023-04-10 15:12:50 -0300

    modificada constante de conexão ao banco de dados

--- backend/api/sql_api/database.py
+++ backend/api/sql_api/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost:5432/app-ensino"
+from ..config import STRINGDB
+
+
+SQLALCHEMY_DATABASE_URL = STRINGDB
 
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # SQLite
